6aef69178ae416acb09b1cbd4d6a916620e9a54a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-03-30 14:19:45 +0100

    individual posts

--- src/components/PostInfo.tsx
+++ src/components/PostInfo.tsx
@@ -21,7 +21,7 @@ export default function Example({ post }: Props) {
       </div>
       <div className="group relative">
         <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-          <a href={''}>
+          <a href={`/posts/${post.slug}`}>
             <span className="absolute inset-0" />
             {title}
           </a>
