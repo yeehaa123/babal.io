bf5a88a564cd96203ec25cf8d5e1deafc37f6ce3
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-04-18 20:14:38 +0200

    cards

---
+++ src/components/Curator.tsx
@@ -0,0 +1,37 @@
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+
+import {
+  CardDescription,
+} from "@/components/ui/card"
+
+interface Props {
+  curator: string;
+}
+
+import { GitHubLogoIcon, LinkedInLogoIcon, InstagramLogoIcon } from '@radix-ui/react-icons'
+
+
+export default function AvatarStack({ curator }: Props) {
+  return (
+    <div className="flex align-middle py-4 items-center justify-between">
+      <div className="flex items-center space-x-3">
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+          <AvatarFallback>YH</AvatarFallback>
+        </Avatar>
+        <CardDescription>{curator}</CardDescription>
+      </div>
+
+
+      <div className="flex items-center space-x-3">
+        <GitHubLogoIcon className="h-6 w-6" />
+        <LinkedInLogoIcon className="h-6 w-6" />
+        <InstagramLogoIcon className="h-6 w-6" />
+      </div>
+    </div>
+  )
+}
