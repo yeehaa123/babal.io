6b7e83d836894a943f6ada75b81a6a2183fc802a
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-04-22 11:11:32 +0200

    refactor

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,24 @@ export type Step = {
   name: string;
   description: string;
 }
+
+export type CoursesResult = {
+  id: string,
+  habitat: string | null,
+  description: string,
+  goal: string,
+  curator: string
+}
+
+export type SocialsResult = {
+  alias: string,
+  linkedin: string,
+}
+
+export type CheckpointsResult = {
+  task: string,
+  href: string
+}
+
+
+
