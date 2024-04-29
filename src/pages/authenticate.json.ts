dcf1387e00b70d3ceb47270f61d91f9b03bcd737
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-04-29 18:06:56 +0200

    add get route to auth

--- src/pages/authenticate.json.ts
+++ src/pages/authenticate.json.ts
@@ -2,6 +2,17 @@ import { authenticate } from "@/db/PeopleDB";
 
 const people = (await authenticate('Yeehaa'))
 
+export async function GET() {
+  return new Response(
+    JSON.stringify({}), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }
+  );
+}
+
 export async function POST() {
   return new Response(
     JSON.stringify(people), {
