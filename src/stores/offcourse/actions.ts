3efaa55ae4d549fa8c18aa60f77de232945d05fa
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-05-11 12:14:32 +0200

    separate actions from store

--- src/stores/offcourse/actions.ts
+++ src/stores/offcourse/actions.ts
@@ -1,34 +1,9 @@
-import { createStore } from 'zustand'
-import type { Course } from "@/types";
-import { produce } from 'immer';
 import type { StoreApi } from "zustand";
-import { combine } from "zustand/middleware";
-import { immer } from 'zustand/middleware/immer'
+import type { Course } from "@/types";
+import type { OffcourseState } from "./"
+import { produce } from 'immer';
 
-
-export interface StoreProps { courses: Course[] }
-
-
-export type OffcourseStore = ReturnType<typeof createOffcourseStore>
-
-type LearnData = {
-  isBookmarked: boolean,
-  tasksCompleted: boolean[]
-}
-
-type LearnDataState = Record<string, LearnData>
-
-export type OffcourseInitialState = {
-  courses: Record<string, Course>,
-  learnData: LearnDataState,
-}
-
-export type OffcourseState = {
-  courses: Record<string, Course>,
-  learnData: LearnDataState,
-  actions: StoreActions
-}
-
+type OffcourseInitialState = Omit<OffcourseState, "actions">
 
 type Api = StoreApi<OffcourseInitialState>;
 
@@ -37,9 +12,9 @@ class StoreActions {
 
   addCourse = () => {
     const courseId = Object.keys(this.get().courses)[0]!;
-    console.log(courseId);
     this.cloneCourse(courseId);
   };
+
   cloneCourse = (courseId: Course['id']) => this.set(produce((state) => {
     if (!courseId) {
       throw ("ERROR")
@@ -64,20 +39,4 @@ class StoreActions {
   }))
 }
 
-
-export function createOffcourseStore({ courses }: StoreProps) {
-  const storeEntries = courses.map(course => {
-    return [course.id, course]
-  });
-
-  const initialState = {
-    courses: Object.fromEntries(storeEntries),
-    learnData: {}
-  }
-  const store = createStore(
-    combine(
-      initialState,
-      (set, get) => ({ actions: new StoreActions(set, get) })))
-  return store;
-}
-
+export { StoreActions }
---
+++ src/stores/offcourse/index.ts
@@ -0,0 +1,36 @@
+import { createStore } from 'zustand'
+import type { Course } from "@/types";
+import { combine } from "zustand/middleware";
+import { StoreActions } from "./actions"
+
+export interface StoreProps { courses: Course[] }
+
+export type OffcourseStore = ReturnType<typeof createOffcourseStore>
+
+type LearnData = {
+  isBookmarked: boolean,
+  tasksCompleted: boolean[]
+}
+
+type LearnDataState = Record<string, LearnData>
+
+export type OffcourseState = {
+  courses: Record<string, Course>,
+  learnData: LearnDataState,
+  actions: StoreActions
+}
+
+export function createOffcourseStore({ courses }: StoreProps) {
+  const storeEntries = courses.map(course => {
+    return [course.id, course]
+  });
+
+  const initialState = {
+    courses: Object.fromEntries(storeEntries),
+    learnData: {}
+  }
+  return createStore(
+    combine(initialState, (set, get) => ({ actions: new StoreActions(set, get) }))
+  )
+}
+
