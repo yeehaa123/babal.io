b38adc5d35c857e4f89dcca1d352828e02b65377
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-05-15 20:39:46 +0200

    more tag magic

--- db/LLMaugmentation.ts
+++ db/LLMaugmentation.ts
@@ -9,7 +9,7 @@ export async function getLLMDescription({ href, task, goal }: TempCheckpoint) {
   const num_words = 300;
   const num_chars = 400;
   const min_num_tags = 1;
-  const max_num_tags = 3;
+  const max_num_tags = 4;
   const tag_length = 7;
 
   const initialMessages: ChatCompletionMessageParam[] = [
@@ -52,7 +52,7 @@ export async function getLLMDescription({ href, task, goal }: TempCheckpoint) {
   }
 
   const tagsMessage: ChatCompletionMessageParam = {
-    role: "user", content: `while keeping the specific goal and task in mind, can you give me a minimum of ${min_num_tags} and a maximum of ${max_num_tags} single-word simple tags for this href? Only include tags that are really important. Again, only give me the answer. No extra words. Tags can have no more than ${tag_length} characters, are not composed of multiple words and can thus not contain a hyphen, and are all lowercase. Format this as a comma separated list`
+    role: "user", content: `while keeping the specific goal and task in mind, can you give me a minimum of ${min_num_tags} and a maximum of ${max_num_tags} single-word, simple, non-hyphenated tags for this href? Only include tags that are really important. Again, only give me the answer. No extra words. Tags can have no more than ${tag_length} characters, are not composed of multiple words and can thus not contain a hyphen, and are all lowercase. Format this as a comma separated list`
   };
 
   const tagsCompletion = await openai.chat.completions.create({
--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -2,6 +2,6 @@ import { Badge } from "@/components/ui/badge"
 
 export function Tags({ tags }: { tags: string[] }) {
   return <div className="flex flex-wrap gap-1">
-    {tags.map(tag => <Badge variant="outline" key={tag}>{tag}</Badge>)}
+    {tags.map(tag => <Badge variant="default" className="bg-gray-50 text-black font-normal hover:bg-black hover:text-white" key={tag}>{tag}</Badge>)}
   </div>
 }
