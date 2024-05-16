f15ec0d63193800a27168047558dbeaec5e54eba
Jan Hein Hoogstad <yeehaa@offcourse.io>
2024-05-16 12:13:42 +0200

    add tag links

--- src/components/Tags.tsx
+++ src/components/Tags.tsx
@@ -1,7 +1,15 @@
-import { Badge } from "@/components/ui/badge"
+import { badgeVariants } from "@/components/ui/badge"
+import { cn } from "@/lib/utils"
 
 export function Tags({ tags }: { tags: string[] }) {
   return <div className="flex flex-wrap gap-1">
-    {tags.map(tag => <Badge variant="default" className="bg-gray-50 text-black font-normal hover:bg-black hover:text-white" key={tag}>{tag}</Badge>)}
+    {tags.map(tag => <a
+      href={`/offcourse/tags/${tag}`}
+      className={cn(
+        badgeVariants({ variant: "outline" }),
+        "text-black font-normal hover:bg-black hover:text-white")}
+      key={tag}>
+      {tag}
+    </a>)}
   </div>
 }
